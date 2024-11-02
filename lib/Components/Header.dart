6874297229b196d4a7a7968825d9878a0cfaa686
Mubashir Ahmed <75428287+mubashir538@users.noreply.github.com>
2024-11-02 15:55:53 +0500

    improved the header image

--- lib/Components/Header.dart
+++ lib/Components/Header.dart
@@ -9,15 +9,16 @@ class Header extends StatelessWidget {
   final String image;
   const Header({this.heading = '', this.para = '', this.image = '', super.key});
 
-  @override
+  @overridem88
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     bool hasSomething =
         heading.isNotEmpty || para.isNotEmpty || image.isNotEmpty;
-
-    bool isSvg = image.substring(image.length - 3) == 'svg' ? true : false;
-
+    bool isSvg = false;
+    if (image.isNotEmpty) {
+      isSvg = image.substring(image.length - 3) == 'svg' ? true : false;
+    }
     double MaximumThing;
     if (screenWidth > screenHeight) {
       MaximumThing = screenWidth;
