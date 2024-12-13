9259d7324c201aac3a2531c414d261b6b96a3191
JawadRazaQadri <jawadraza1406@gmail.com>
2024-12-13 19:29:24 +0500

    Cateogary Add Image design done

--- lib/Screens/AddCategory/AddCategory_AddImage.dart
+++ lib/Screens/AddCategory/AddCategory_AddImage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:taqreeb/Components/header.dart';
-import 'package:taqreeb/Components/text_box.dart';
 import 'package:taqreeb/theme/color.dart';
 import 'package:taqreeb/theme/icons.dart';
 
@@ -26,63 +24,216 @@ class AddImage extends StatelessWidget {
               margin: EdgeInsets.all(MaximumThing * 0.01),
               height: screenHeight * 0.3,
               width: screenWidth * 0.9,
+              padding: EdgeInsets.symmetric(
+                  horizontal: MaximumThing * 0.03,
+                  vertical: MaximumThing * 0.02),
               decoration: BoxDecoration(
                 color: MyColors.DarkLighter,
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.4),
-                    blurRadius: 4,
-                    spreadRadius: 1,
-                    offset: Offset(2, 2),
-                  ),
+                      color: Colors.black.withOpacity(0.4),
+                      blurRadius: 4,
+                      spreadRadius: 1,
+                      offset: Offset(2, 2))
                 ],
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(MaximumThing * 0.01),
-              height: screenHeight * 0.3,
-              width: screenWidth * 0.9,
-              decoration: BoxDecoration(
-                color: MyColors.DarkLighter,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.4),
-                    blurRadius: 4,
-                    spreadRadius: 1,
-                    offset: Offset(2, 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: MyColors.DarkLighter,
+                    child: Image.asset(
+                      MyIcons.add,
+                      color: MyColors.white,
+                    ),
                   ),
                 ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
-                ),
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                CircleAvatar(
-                  radius: 20,
-                  backgroundColor: MyColors.white,
-                  child: Image.asset(
-                    MyIcons.add,
+                Padding(
+                  padding: EdgeInsets.only(left: MaximumThing * 0.02),
+                ),
+                Container(
+                  margin: EdgeInsets.all(MaximumThing * 0.01),
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.4,
+                  decoration: BoxDecoration(
                     color: MyColors.DarkLighter,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: MaximumThing * 0.03),
+                ),
+                Container(
+                  margin: EdgeInsets.all(MaximumThing * 0.01),
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.4,
+                  decoration: BoxDecoration(
+                    color: MyColors.DarkLighter,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: MaximumThing * 0.02),
+                ),
+                Container(
+                  margin: EdgeInsets.all(MaximumThing * 0.01),
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.4,
+                  decoration: BoxDecoration(
+                    color: MyColors.DarkLighter,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: MaximumThing * 0.03),
+                ),
+                Container(
+                  margin: EdgeInsets.all(MaximumThing * 0.01),
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.4,
+                  decoration: BoxDecoration(
+                    color: MyColors.DarkLighter,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: MaximumThing * 0.02),
+                ),
+                Container(
+                  margin: EdgeInsets.all(MaximumThing * 0.01),
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.4,
+                  decoration: BoxDecoration(
+                    color: MyColors.DarkLighter,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: MaximumThing * 0.03),
+                ),
+                Container(
+                  margin: EdgeInsets.all(MaximumThing * 0.01),
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.4,
+                  decoration: BoxDecoration(
+                    color: MyColors.DarkLighter,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5DJA0WgEFo7X9kXf00EtVnpGPD3mAvh1e8A&s',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
                   ),
                 ),
               ],
