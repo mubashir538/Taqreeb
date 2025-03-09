406cefa657aa28bd578e59d4d14bfb74d9cb5086
Wishma Khan <wishmak78@gmail.com>
2025-03-09 22:53:57 +0500

    errors

--- Backend/Backend/config.py
+++ Backend/Backend/config.py
@@ -4,5 +4,5 @@ def get_public_ip():
     response = requests.get('https://api.ipify.org?format=json')
     return response.json()['ip']
 
-ip = 'https://984f-182-176-107-231.ngrok-free.app'
-# ip = 'http://192.168.0.104'
+#ip = 'https://984f-182-176-107-231.ngrok-free.app'
+ip = 'http://192.168.100.15'
