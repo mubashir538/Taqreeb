69385b348f4b679342b09bf6e128f4462eba0532
Mubashir Ahmed <75428287+mubashir538@users.noreply.github.com>
2025-04-28 02:56:43 +0500

    imported timezone in payment

--- Backend/Backend/myapp/apis/Payment.py
+++ Backend/Backend/myapp/apis/Payment.py
@@ -1,6 +1,5 @@
 import datetime
 from decimal import Decimal
-
 from django.shortcuts import get_object_or_404
 from sympy import Q
 from .. import models as m
@@ -9,6 +8,7 @@ from django.db import transaction
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
+from django.utils import timezone
 
 
 # views.py
